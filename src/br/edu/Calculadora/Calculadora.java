483a0fe005fb5d6a78827e2171a4b38a905d54bc
Tecnico - TI <tecnico@epfserver.com.br>
2016-05-02 22:43:07 -0300

    Inclusoa da funcao soma

---
+++ src/br/edu/Calculadora/Calculadora.java
@@ -0,0 +1,30 @@
+package br.edu.Calculadora;
+public class Calculadora {
+
+
+
+	private int valor;
+
+	public void somar (int valorUm, int valorDois){		
+		this.valor = this.valor + (valorUm + valorDois);
+
+	
+	}
+
+	public void subtrair(){
+
+	}
+
+	public void dividir(){
+
+	}
+
+	public void multiplicar(){
+
+	}
+
+	public int getValor(){
+		return valor;
+	}
+
+}
---
+++ src/br/edu/Calculadora/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.Calculadora;
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Calculadora calculadoraDoPedrinho = new Calculadora();
+
+		calculadoraDoPedrinho.somar(10,50);
+		calculadoraDoPedrinho.somar(20,10);
+		JOptionPane.showMessageDialog(null, calculadoraDoPedrinho.getValor());
+
+	}
+
+
+}
